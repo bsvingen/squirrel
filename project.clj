5d81deef289c53fc4dc2c7fe9d4bb2dddcff68a7
Børge Svingen <bsvingen@borkdal.com>
2015-07-02 00:31:50 +0200

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.borkdal/squirrel "0.3.0"
+(defproject com.borkdal/squirrel "0.3.1-SNAPSHOT"
   :description "SQuirreL SQL library"
   :url "https://github.com/bsvingen/squirrel"
   :license {:name "Apache License 2.0"
