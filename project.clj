6138b6ba12b466fe740ada43c87872accc00a1b6
Børge Svingen <bsvingen@borkdal.com>
2015-08-12 22:18:07 +0200

    Updating midje version

--- project.clj
+++ project.clj
@@ -7,11 +7,7 @@
                  [potemkin "0.3.7"]
                  [org.tobereplaced/lettercase "1.0.0"]
                  [com.borkdal/clojure.utils "0.1.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [joda-time
-                                                              org.clojars.trptcolin/sjacket]]
-                                  [clj-time "0.9.0"]
-                                  [org.clojars.trptcolin/sjacket "0.1.0.6"
-                                   :exclusions [org.clojure/clojure]]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :plugins [[codox "0.8.10" :exclusions [org.clojure/clojure]]
             [midje-readme "1.0.8"]]
